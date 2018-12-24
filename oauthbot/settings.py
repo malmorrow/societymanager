1e74c0aeb2c7064a8b72b6213ef870fb5c0073a3
Mal Morrow <malmorrow@gmail.com>
2018-12-25 00:13:26 +0200

    Changed logic of PROJECT_NAME.

--- oauthbot/settings.py
+++ oauthbot/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 
 # Identify the current project by name
-PROJECT_NAME = os.getcwd().split('/')[-1]
+PROJECT_NAME = os.path.dirname(os.path.realpath(__file__)).split('/')[-1]
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
