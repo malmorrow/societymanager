c2f11a29e9a9101e85f9498e1676b1de85bb816a
Mal Morrow <malmorrow@gmail.com>
2018-12-29 17:05:01 +0200

    Added some /regsiter functionality.

--- users/models.py
+++ users/models.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class CustomUserManager(UserManager):
+	
 	pass
 
 
 class CustomUser(AbstractUser):
+
 	objects = CustomUserManager()
 
